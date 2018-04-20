5d2baf7a8fa3ac096e47deba3d42285eea061d68
neerajbisht1987 <bisht.neeraj@gmail.com>
2018-04-20 11:16:55 +0530

    added test cases and properly

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,10 @@ pub fn run(config:ConfigInfo)->Result<(),Box<Error>>
 	let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);	 
+    for line in search(&config.query,&contents)
+    {
+    println!("{}", line);	
+    }
 	Ok(())
 }
 
@@ -28,4 +31,28 @@ pub fn new(args:&[String])->Result<ConfigInfo,&'static str>{
     let filename = args[2].clone();
 	Ok(ConfigInfo{query,filename})
 }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> { 
+    let mut results = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+fn one_result() {
+let query = "fast"; let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+assert_eq!(
+vec!["safe, fast, productive."], search(query, contents)
+); }
 }
